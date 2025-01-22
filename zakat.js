09bc4a006da89a80682ca11df30c00d3006883b9
RabiaKhalidButt <Rabia_khalid9@hotmail.com>
2025-01-23 03:03:22 +0500

    Add files via upload

---
+++ zakat.js
@@ -0,0 +1,8 @@
+let  naam=prompt("Enter Your Name" )
+function abcd(){
+let zktamount= 2.5
+let totamount=document.getElementById("tot1").value;
+let res= totamount*zktamount/100
+document.write(`dear ${naam} your courrent zakat amount is ${res}`)
+console.log(`dear ${naam} your courrent zakat amount is ${res}`)
+}
